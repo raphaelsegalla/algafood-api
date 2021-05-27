938c61e460e3c5b3dc741c101828103774f49335
Raphael Fernando Pimentel Segalla <raphael.segalla@gmail.com>
2021-05-27 17:10:45 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,8 +2,30 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+
+    Optional<Restaurante> findFirstByNomeContaining(String nome);
+
+    Optional<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinhaId);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+
+    List<Restaurante> consultarPorNomeXml(String nome, @Param("id") Long cozinhaId);
+
 }
