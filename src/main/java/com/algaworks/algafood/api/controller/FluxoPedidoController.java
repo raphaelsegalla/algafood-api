636fd2c3258c9ede54f2ecb5733380c54322d83e
Raphael Fernando Pimentel Segalla <raphael.segalla@gmail.com>
2021-08-18 19:35:55 -0300

    Refatorando o código de regras para transição de status de pedidos

---
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.service.FluxoPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/pedidos/{pedidoId}")
+public class FluxoPedidoController {
+
+    @Autowired
+    private FluxoPedidoService fluxoPedidoService;
+
+    @PutMapping("/confirmacao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void confirmar(@PathVariable Long pedidoId) {
+        fluxoPedidoService.confirmar(pedidoId);
+    }
+
+    @PutMapping("/entrega")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void entregar(@PathVariable Long pedidoId) {
+        fluxoPedidoService.entregar(pedidoId);
+    }
+
+    @PutMapping("/cancelamento")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelar(@PathVariable Long pedidoId) {
+        fluxoPedidoService.cancelar(pedidoId);
+    }
+
+}
--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -66,11 +67,26 @@ public class Pedido {
         this.valorTotal = this.subtotal.add(this.taxaFrete);
     }
 
-    public void definirFrete() {
-        setTaxaFrete(getRestaurante().getTaxaFrete());
+    public void confirmar() {
+        setStatus(StatusPedido.CONFIRMADO);
+        setDataConfirmacao(OffsetDateTime.now());
     }
 
-    public void atribuirPedidoAosItens() {
-        getItens().forEach(item -> item.setPedido(this));
+    public void entregar() {
+        setStatus(StatusPedido.ENTREGUE);
+        setDataEntrega(OffsetDateTime.now());
     }
+
+    public void cancelar() {
+        setStatus(StatusPedido.CANCELADO);
+        setDataCancelamento(OffsetDateTime.now());
+    }
+
+    private void setStatus(StatusPedido novoStatus) {
+        if (getStatus().nãoPodeAlterarPara(novoStatus)) {
+            throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %s para %s", getId(), getStatus().getDescricao(), novoStatus.getDescricao()));
+        }
+        this.status = novoStatus;
+    }
+
 }
