2f10885cabeb68df1ad245e3024a9def377615cf
Raphael Fernando Pimentel Segalla <raphael.segalla@gmail.com>
2021-09-29 21:36:03 -0300

    Ajuste instanciação do bean amazons3

--- src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
+++ src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
@@ -8,6 +8,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +21,7 @@ public class StorageConfig {
     private StorageProperties storageProperties;
 
     @Bean
+    @ConditionalOnProperty(name = "algafood.storage.tipo", havingValue = "s3")
     public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(storageProperties.getS3().getIdChaveAcesso(), storageProperties.getS3().getChaveAcessoSecreta());
 
