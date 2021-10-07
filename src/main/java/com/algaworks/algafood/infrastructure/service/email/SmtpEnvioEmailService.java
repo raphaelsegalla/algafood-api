0df26ad0b1095945e9efd50d0a3ef14ebbb22d18
Raphael Fernando Pimentel Segalla <raphael.segalla@gmail.com>
2021-10-06 22:11:18 -0300

    implementando serviço de envio de e-mail fake

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -2,14 +2,17 @@ package com.algaworks.algafood.infrastructure.service.email;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.internet.MimeMessage;
 
-@Service
+//@Service
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
     @Autowired
@@ -18,21 +21,36 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     @Autowired
     private EmailProperties emailProperties;
 
+    @Autowired
+    private Configuration freemarkerConfig;
+
     @Override
     public void enviar(Mensagem mensagem) {
         try {
+            String corpo = processarTemplate(mensagem);
+
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setFrom(emailProperties.getRemetente());
             helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
             helper.setSubject(mensagem.getAssunto());
-            helper.setText(mensagem.getCorpo(), true);
+            helper.setText(corpo, true);
 
             javaMailSender.send(mimeMessage);
         } catch (Exception e) {
-            throw new EmailException("Não foi possivel enviar email", e);
+            throw new EmailException("Não foi possivel enviar e-mail", e);
         }
+    }
+
+    protected String processarTemplate(Mensagem mensagem) {
+        try {
+            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template,mensagem.getVariaveis());
+        } catch (Exception e) {
+            throw new EmailException("Não foi possivel montar o template do e-mail", e);
+        }
+
     }
 
 }
