2efb730a3fec2117925799bf3c18ac8b86602259
Raphael Fernando Pimentel Segalla <raphael.segalla@gmail.com>
2021-10-12 21:24:36 -0300

    Implementando uma requisição POST no client Java

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -33,7 +33,6 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
