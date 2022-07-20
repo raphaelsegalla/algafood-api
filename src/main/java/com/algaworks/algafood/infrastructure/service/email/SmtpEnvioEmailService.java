db25044b76851acba4390e8f33e7a697c42441d5
rsegalla <rsegalla@uolinc.com>
2022-07-20 12:15:39 -0300

    Dependência de JavaMailSender não satisfeita: melhorando o uso da herança

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -23,7 +23,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     private EmailProperties emailProperties;
 
     @Autowired
-    private Configuration freemarkerConfig;
+    private ProcessadorEmailTemplate processadorEmailTemplate;
 
     @Override
     public void enviar(Mensagem mensagem) {
@@ -37,7 +37,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     }
 
     protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-        String corpo = processarTemplate(mensagem);
+        String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
@@ -48,16 +48,6 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
         helper.setText(corpo, true);
 
         return mimeMessage;
-    }
-
-    protected String processarTemplate(Mensagem mensagem) {
-        try {
-            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
-            return FreeMarkerTemplateUtils.processTemplateIntoString(template,mensagem.getVariaveis());
-        } catch (Exception e) {
-            throw new EmailException("Não foi possivel montar o template do e-mail", e);
-        }
-
     }
 
 }
